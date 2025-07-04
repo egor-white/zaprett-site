74c46149abf23e6bac441d7ff8bff78b3093c114
egor-white <channela082@gmail.com>
2025-07-04 21:25:12 +0300

    update config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "zaprett",
   base: "/zaprett-site/",
+  cleanUrls: true,
   description: "Документация zaprett",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
